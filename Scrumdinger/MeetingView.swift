3c3112ffaa98b435d5bebdf45a14bee9525cc9b9
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2023-05-17 11:27:51 +0500

    first

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Kuziboev Siddikjon on 12/05/23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: 10, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Second Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Second Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle().stroke(lineWidth: 12)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
